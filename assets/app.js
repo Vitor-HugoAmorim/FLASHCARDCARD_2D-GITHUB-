05f3d56e3f958669c86a68be7f8c1edbf8402e10
Vitor-HugoAmorim <162454490+Vitor-HugoAmorim@users.noreply.github.com>
2024-10-10 07:57:24 -0300

    .

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+function criaCartao(Categoria, Pergunta, Resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = cartao
+}
---
+++ assets/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Em que ano foi declarada a indepêndencia?',
+    '1822'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 1089?',
+    '33'
+)
+
+criaCartao(
+    'Futbol',
+    'Qual é o melhor jogador da história?',
+    'Pelé'
+)
+
+criaCartao(
+    'Games',
+    'Qual o video game mais marcante da história?',
+    'Playstation 2'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual melhor filme de comédia produzido no Brasil?',
+    'O auto da compadecida'
+)
+
+criaCartao(
+    'Futbol',
+    'Em que ano o Brasil ganhou o primeiro ouro em futbol nas olimpiadas?',
+    '2016'
+)
