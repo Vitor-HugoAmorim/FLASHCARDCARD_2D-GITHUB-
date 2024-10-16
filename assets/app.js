21a1a3059b5b3847c8bac85d18709a7c13a71323
Vitor-HugoAmorim <162454490+Vitor-HugoAmorim@users.noreply.github.com>
2024-10-16 11:28:11 -0300

    .

--- assets/app.js
+++ assets/app.js
@@ -2,15 +2,26 @@ function criaCartao(Categoria, Pergunta, Resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = cartao
-}
 
-cartao.innerHTML = `
+    cartao.innerHTML = `
  <div class="cartao_conteudo">
-                    <h3>Programação</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao_conteudo_pergunta">
-                        <p>O que é JavaScript?</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="Cartao_conteudo_resposta">
-                        <p>O JavaScript é uma linguagem de Programação</p>
+                        <p>${resposta}</p>
                     </div>
                 </div>
+                `
+
+    let respostaEstaVisivel = false 
+
+    function ViraCartao () {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', ViraCartao)
+
+    container.appendChild(cartao)
+}
